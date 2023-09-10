b8aff23eb8d2f23f4c84b2a55ed9da69ea53499d
msuleiman-Dev <95286033+msuleiman-Dev@users.noreply.github.com>
2023-09-10 16:56:17 +0100

    2i_Commit

---
+++ ArrayLookUp.cs
@@ -0,0 +1,27 @@
+public static class ArrayLookUp
+{
+    public static int GetTotalNumberOfSums(int targetSum, int[] array)
+    {
+        var counter = 0;
+        if (targetSum > 0 && array.Length > 1)
+        {
+            var hashedList = array.ToHashSet();
+            foreach (var number in hashedList)
+            {
+                if (number * 2 > targetSum)
+                {
+                    continue;
+                }
+
+                var missingSumPart = targetSum - number;
+                if (missingSumPart != number && hashedList.Contains(missingSumPart))
+                {
+                    counter++;
+                }
+
+            }
+
+        }
+        return counter;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+    using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("ConcoleAppTests")]
+namespace ConcoleApp
+{
+
+
+internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            var targetArray = new[] { 1, 3, 5, 8, 10 };
+            var targetSum = 13;
+            var totalSumsFound = ArrayLookUp.GetTotalNumberOfSums(targetSum, targetArray);
+            Console.WriteLine($"X{0}", targetSum);
+            Console.WriteLine($"Array:{0}", targetArray.ToString());
+            Console.WriteLine($"Output:{0}", totalSumsFound);
+
+        }
+    }
+}
