c48745c4997479c3dd2ff85a5a0a50970ec1f21e
msuleiman-Dev <95286033+msuleiman-Dev@users.noreply.github.com>
2023-09-17 23:20:09 +0100

    Code refactoring

---
+++ ArrayUtil.Tests/GetTotalSumsTest.cs
@@ -0,0 +1,24 @@
+using System.Text;
+using NUnit.Framework;
+
+namespace ArrayUtil.Tests;
+[TestFixture]
+public class GetTotalSumTest
+{
+    int[] _arrayToTest;
+    int _targetTestSum;
+    [SetUp]
+    public void Setup()
+    {
+        _arrayToTest = new int[] { 1, 3, 5, 8, 10 };
+        _targetTestSum = 13;
+    }
+
+    [Test]
+    public void GetTotalSumValueTest()
+    {
+        var result = Iterator.GetTotalNumberOfSums(_targetTestSum, _arrayToTest);
+        Assert.That(result, Is.EqualTo(2));
+       
+    }
+}
---
+++ ArrayUtil/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("ArrayUtil.Tests")]
+namespace ArrayUtil;
+internal class Program
+{
+    internal static void Main(string[] args)
+    {
+        var targetArray = new[] { 1, 3, 5, 8, 10 };
+        var targetSum = 13;
+        var totalSumsFound = Iterator.GetTotalNumberOfSums(targetSum, targetArray);
+        Console.WriteLine($"X{0}", targetSum);
+        Console.WriteLine($"Array:{0}", targetArray.ToString());
+        Console.WriteLine($"Output:{0}", totalSumsFound);
+    }
+}
